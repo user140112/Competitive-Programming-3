13b6730e22314937be40e19102f1d2483ebb3cdf
user140112 <shakilsorkar14@gmail.com>
2018-10-14 12:49:54 +0600

    usage of clear()

---
+++ String Processing-ch6/ch6_6.0.0/input_char_and_clear.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string str;
+    char c;
+
+    do{
+       c=cin.get();
+       if(c!='.') str+=c;
+    }while(c!='.');
+
+    cout<<"String before clear: "<<str<<endl;
+    str.clear();
+    cout<<"String after clear: "<<str<<endl;
+    return 0;
+}
