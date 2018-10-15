1d103cbe7b25341facf64fe5872106926f1ae3ff
user140112 <shakilsorkar14@gmail.com>
2018-10-15 12:41:16 +0600

    How to stroe string in C++

---
+++ String Processing-ch6/ch6_6.2.1.a.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str1="Competitive Programming";
+    string str2("ACM ICPC");
+    string str3(str1);
+    cout<<str1<<endl<<str2<<endl<<str3<<endl;
+    return 0;
+}
+
+/**
+Output:
+-------
+
+Competitive Programming
+ACM ICPC
+Competitive Programming
+
+**/
