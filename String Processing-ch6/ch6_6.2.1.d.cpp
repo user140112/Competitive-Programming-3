0175d17c0cb2117a6c297dd9511aa76fa7865f93
user140112 <shakilsorkar14@gmail.com>
2018-10-15 12:45:52 +0600

    Check whether a string strats with 7 dots or not

---
+++ String Processing-ch6/ch6_6.2.1.d.cpp
@@ -0,0 +1,29 @@
+/**
+Following program, we can check whether a string start with 7 dots or not.
+If the string start with 7 dots, we will break the program.
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str;
+    getline(cin,str);
+    if(str.compare(0,7,".......")==0){
+        cout<<"The string contains 7 dots in the beginning"<<endl;
+    }else{
+        cout<<"The string doest not contain 7 dots in the beginning"<<endl;
+    }
+
+    return 0;
+}
+/**
+
+Input=  .......prime number
+Output= The string contains 7 dots in the beginning
+
+Input=
+Output=
+
+**/
