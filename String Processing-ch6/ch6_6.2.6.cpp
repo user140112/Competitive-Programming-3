19b07ae5e07703ca89685b7bd0e5f1c2c9714147
user140112 <shakilsorkar14@gmail.com>
2018-10-16 01:30:29 +0600

    Count alphabetic from unknown length's string

---
+++ String Processing-ch6/ch6_6.2.6.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char ch;
+    int alphas=0;
+    while(scanf("%c",&ch),ch!='\n'){
+        alphas += isalpha(ch) ? 1 : 0;;
+    }
+    cout<<alphas<<endl;
+    return 0;
+}
+/**
+Output:
+------
+jessore university of science and technology.
+39
+
+**/
