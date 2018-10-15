36193df75a1bc2937dafe27e3def7579801aea6e
user140112 <shakilsorkar14@gmail.com>
2018-10-15 12:42:47 +0600

    Text input line by line in C++

---
+++ String Processing-ch6/ch6_6.2.1.b.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str;
+    while(getline(cin,str)){
+        if(str.size()==0) break;
+        cout<<str<<endl;
+    }
+    return 0;
+}
+
+/**
+Output:
+------
+jessore university of science and technology
+jessore university of science and technology
+Bangladesh
+Bangladesh
+
+Note: Press double enter to terminate the program.
+**/
