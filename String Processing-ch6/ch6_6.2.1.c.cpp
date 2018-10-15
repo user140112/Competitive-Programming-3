dd3773ba73e6716ccc9f851a3ae48274063d235d
user140112 <shakilsorkar14@gmail.com>
2018-10-15 12:43:54 +0600

    How to concatenate two string in C++

---
+++ String Processing-ch6/ch6_6.2.1.c.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str1,str2,str3;
+    getline(cin,str1);
+    getline(cin,str2);
+    str3=str1+" "+str2;
+    cout<<str3<<endl;
+    return 0;
+}
+
+/**
+Output:
+-------
+
+Dhaka
+Bangladesh
+Dhaka Bangladesh
+
+**/
